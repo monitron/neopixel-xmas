77a328ef2ea44802aec1022dc7603bae3f1ce704
Justin Streufert <monitron@gmail.com>
2014-12-20 22:04:12 -0500

    Initial code

---
+++ neopixel-xmas.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+#define LEDS 150
+
+void setup() {
+  // put your setup code here, to run once:
+  Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
